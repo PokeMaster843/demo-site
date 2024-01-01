1bdd9edc6f3f3f2a09a43409a01b9dee8fe9d787
PokeMaster843 <pokemaster843@gmail.com>
2024-01-01 14:01:06 -0500

    Added scroll behavior for navigating to specific page content. Updated example demo page paths.

--- src/router.js
+++ src/router.js
@@ -4,9 +4,18 @@ import AboutMe from "@/components/pages/AboutMe.vue";
 import WhyVue from "@/components/pages/WhyVue.vue";
 import MyResume from "@/components/pages/MyResume.vue";
 import SkillDemo from "@/components/pages/SkillDemo.vue";
+import DemoA from "@/components/pages/demos/DemoA.vue";
 
 export const router = createRouter({
 	history: createWebHashHistory(),
+    // eslint-disable-next-line no-unused-vars
+    scrollBehavior(to, from, savePosition) {
+        if(to.hash) {
+            return {
+                el: to.hash
+            };
+        }
+    },
 	routes: [
         {
             path: "/",
@@ -38,9 +47,19 @@ export const router = createRouter({
             component: SkillDemo,
             children: [
                 {
-                    path: "demo1",
-                    name: "demo1",
-                    component: {template: ""}
+                    path: "demo-a",
+                    name: "demoA",
+                    component: DemoA
+                },
+                {
+                    path: "demo-b",
+                    name: "demoB",
+                    component: DemoA
+                },
+                {
+                    path: "demo-c",
+                    name: "demoC",
+                    component: DemoA
                 }
             ]
         }

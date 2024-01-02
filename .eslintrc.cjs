be18dc0797fda48590e5dd233590b12faf44dae8
PokeMaster843 <pokemaster843@gmail.com>
2024-01-02 16:25:26 -0500

    Add configuration for component testing with Cypress.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,8 @@ module.exports = {
     {
       files: [
         'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-        'cypress/support/**/*.{js,ts,jsx,tsx}'
+        'cypress/support/**/*.{js,ts,jsx,tsx}',
+        'src/components/__tests__/**/*.{cy,spec}.{js,ts,jsx,tsx}'
       ],
       'extends': [
         'plugin:cypress/recommended'

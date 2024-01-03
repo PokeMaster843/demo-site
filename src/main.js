40f669e726090bd90d558c6a723c6aae3e30b483
PokeMaster843 <pokemaster843@gmail.com>
2024-01-03 16:21:26 -0500

    Added Pinia to the project.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,23 @@
 import "@/assets/main.scss";
 
+// Vue
 import { createApp } from "vue";
 import App from "@/App.vue";
+
+// VueRouter
 import { router } from "@/router";
 
+// Vuetify
 import "@mdi/font/css/materialdesignicons.css";
 import "vuetify/styles";
 import vuetify from "@/plugins/vuetify";
 
+// Pinia
+import { createPinia } from "pinia";
+
+const pinia = createPinia();
 const app = createApp(App);
 app.use(vuetify);
 app.use(router);
+app.use(pinia);
 app.mount("#app");

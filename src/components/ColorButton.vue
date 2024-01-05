9311931da13ce2123042081371aa9e31405c7711
PokeMaster843 <pokemaster843@gmail.com>
2024-01-05 13:39:42 -0500

    Created custom ColorButton component.

---
+++ src/components/ColorButton.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { useThemeStore } from "@/stores/theme";
+
+const store = useThemeStore();
+
+function toCSS(col) {
+    if(/^#(\d|[a-f|A-F]){6}$/.test(col)) {
+        return col;
+    }
+    return "rgb(var(--v-theme-" + col + "))";
+}
+
+defineProps({
+    icon: {
+        type: String,
+        default: ""
+    },
+    size: {
+        type: Number,
+        default: 56
+    },
+    color: {
+        type: String,
+        default: "surface"
+    },
+    title: {
+        type: String,
+        default: ""
+    },
+    selected: {
+        type: Boolean,
+        default: false
+    },
+    activator: {
+        type: Boolean,
+        default: false
+    }
+})
+</script>
+
+<template>
+    <v-btn
+    :class="{selected: selected, activator: activator}"
+    class="ma-2"
+    :elevation="activator ? '2' : '1'"
+    density="comfortable"
+    :icon="icon"
+    :size="size"
+    :color="color"
+    :title="title">
+        <template v-slot:default>
+            <v-icon :color="store.color"></v-icon>
+        </template>
+    </v-btn>
+</template>
+
+<style scoped>
+.activator {
+    --bg-color: v-bind(toCSS(color));
+    box-shadow: 0px 0px 2px 2px var(--bg-color) !important;
+}
+
+.selected {
+    box-shadow: 0px 0px 8px 4px rgb(var(--v-theme-highlight)) !important;
+}
+</style>

4baa578bbab1d1fdf42a5997fc3a8f7dc03b9c5d
PokeMaster843 <pokemaster843@gmail.com>
2024-01-19 15:59:26 -0500

    Added unit tests for IconButton component.

---
+++ src/components/__tests__/IconButton.cy.jsx
@@ -0,0 +1,38 @@
+import IconButton from "@/components/IconButton.vue";
+
+describe("<IconButton />", () => {
+    it("renders with default props", () => {
+        // see: https://on.cypress.io/mounting-vue
+        cy.mount(() => (
+            <IconButton></IconButton>
+        ));
+
+        cy.getById("icon-btn")
+        .should("have.css", "width", "56px")
+        .should("have.css", "height", "56px")
+        .should("have.class", "bg-surface")
+        .should("not.have.attr", "value");
+    });
+
+    it("renders with custom props", () => {
+        cy.mount(() => (
+            <IconButton
+            icon="mdi-circle"
+            icon-color="amber"
+            size="64"
+            color="indigo"
+            value="test">
+            </IconButton>
+        ));
+
+        cy.getById("icon-btn")
+        .should("have.css", "width", "64px")
+        .should("have.css", "height", "64px")
+        .should("have.class", "bg-indigo")
+        .should("have.value", "test");
+        
+        cy.getById("icon-btn-icon")
+        .should("have.class", "mdi-circle")
+        .should("have.class", "text-amber");
+    });
+});

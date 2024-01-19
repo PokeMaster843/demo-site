f00e4ab486c49d473b78ab7e1dfc1b06efed38c5
PokeMaster843 <pokemaster843@gmail.com>
2024-01-19 15:57:31 -0500

    Refactored to JSX.

--- src/components/__tests__/AppBar.cy.jsx
+++ src/components/__tests__/AppBar.cy.jsx
@@ -3,11 +3,10 @@ import AppBar from "@/components/AppBar.vue";
 describe("<AppBar />", () => {
     it("renders with page title", () => {
         // see: https://on.cypress.io/mounting-vue
-        cy.mount(AppBar, {
-            props: {
-                pageTitle: "Test Page Title"
-            }
-        });
+        cy.mount(() => (
+            <AppBar page-title="Test Page Title">
+            </AppBar>
+        ));
 
         cy.getById("page-title")
         .should("have.text", "Test Page Title");
@@ -15,11 +14,10 @@ describe("<AppBar />", () => {
 
     it("button for controlling sidebar does not appear on larger screens", () => {
         cy.desktop();
-        cy.mount(AppBar, {
-            props: {
-                pageTitle: "Test Page Title"
-            }
-        });
+        cy.mount(() => (
+            <AppBar page-title="Test Page Title">
+            </AppBar>
+        ));
 
         cy.getById("sm-menu")
         .should("not.exist");
@@ -27,23 +25,22 @@ describe("<AppBar />", () => {
 
     it("button for controlling sidebar shows up on smaller screens", () => {
         cy.mobile();
-        cy.mount(AppBar, {
-            props: {
-                pageTitle: "Test Page Title"
-            }
-        });
+        cy.mount(() => (
+            <AppBar page-title="Test Page Title">
+            </AppBar>
+        ));
 
         cy.getById("sm-menu")
         .should("exist");
     });
 
     it("app bar extension renders when prop is set", () => {
-        cy.mount(AppBar, {
-            props: {
-                pageTitle: "Test Page Title",
-                includeExtension: true
-            }
-        });
+        cy.mount(() => (
+            <AppBar
+            page-title="Test Page Title"
+            include-extension>
+            </AppBar>
+        ));
 
         cy.get("div .v-toolbar__extension")
         .should("exist");

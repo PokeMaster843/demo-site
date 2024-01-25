53114bceae7a6a15cc00feee0b9fe31d148e7b29
PokeMaster843 <pokemaster843@gmail.com>
2024-01-25 13:27:52 -0500

    Refactored AboutMe page to utilize new 'page' store. Started some styling changes.

--- src/components/pages/AboutMe.vue
+++ src/components/pages/AboutMe.vue
@@ -1,105 +1,117 @@
 <script setup>
 // library imports
-import { useDisplay } from "vuetify";
 
 // project imports
 import PageContent from "@/components/PageContent.vue";
 import photoSelf from "@/assets/photo-self.jpg";
-import { useCarouselStore } from "@/stores/carousel";
-import { aboutPage as page } from "@/references";
+import photoMath from "@/assets/n_pendulum.png";
+import { usePageStore } from "@/stores/page";
 
-const store = useCarouselStore();
-const { xs, width } = useDisplay();
+const store = usePageStore();
+
+const hobbyCarousel = [
+    {
+        src: "https://cdn.vuetifyjs.com/images/parallax/material.jpg",
+        desc:  "For the past 3 years, I have been completely and utterly obsessed with Beat Saber. It served as \
+                my introduction to both VR and rhythm games, two things I now find very fascinating."
+    },
+    {
+        src: photoMath,
+        desc:  "Since I was a young child, I have loved mathematics and numbers. Rather than playing outside, \
+                I would play with the world of mathematics. I simply followed the rabbit hole as far as my \
+                curiosity took me. For example, my interest in making games led me to learn all about the \
+                techniques behind 3D rendering, and my insatiable desire to understand led me to learn about \
+                matrices and quaternions. Of course, I won't say I effectively understood them; I simply gained \
+                enough understanding to use them in basic operations. My curiosity only grew from that point, \
+                but as classes got more intensive in high school and college, I had less free time to spend on \
+                this hobby. Despite this, the skills it built remain: problem solving, data analysis, and a fair \
+                bit of intuition, to name a few."
+    },
+    {
+        src: "https://cdn.vuetifyjs.com/images/parallax/material.jpg",
+        desc:  "Coding/programming was a hobby of mine that developed out of my interest in making games. \
+                In particular, my deep interest in Minecraft and making Minecraft mods led me to start learning \
+                Java in middle school. Whenever I got home from school, I would hop on the computer and watch \
+                YouTube tutorials covering the basics of the language. Little by little, the amount of code \
+                I could write on my own increased. Eventually, I learned how to make Minecraft mods, my original \
+                objective. During this journey, however, I discovered that I loved coding in general. When we \
+                were eventually given Chromebooks in school, I taught myself JavaScript so I could do some \
+                coding at school as well. In fact, that became my main interaction with coding. Furthermore, \
+                my hobbies of coding and mathematics often overlapped, as most projects I worked on were things \
+                like a double pendulum simulator, a 2D physics engine, or even Conway's game of life."
+    }
+];
 </script>
 
 <template>
     <page-content
-    :page-title="page.title"
-    :finished="page.finished">
-        <!--TODO: make banner with image and text saying "Hello, I'm John Knickerbocker."-->
+    :page-title="store.aboutPage.title"
+    :finished="store.aboutPage.finished">
         <v-row>
-            <v-col
-            cols="12"
-            sm="5"
-            md="4"
-            lg="3"
-            style="text-align: center;">
-                <v-img
-                :src="photoSelf"
-                style="border-radius: 50%; margin: auto;"
-                :width="store.carouselHeight(width)-56">
-                </v-img>
+            <v-col>
+            </v-col>
+        </v-row>
 
-                <div
-                class="text-subtitle-1"
-                style="font-style: italic;">
-                    Hello, I'm John Knickerbocker! Welcome to my site!
-                    <template v-if="xs">
-                        I hope the mobile/small-screen version is to your liking!
-                    </template>
+        <v-row
+        class="ml-n6 pl-3 bg-surface"
+        align="center"
+        style="clip-path: polygon(0 0, 80% 0%, 75% 100%, 0% 100%);">
+            <v-col cols="3">
+                <v-img
+                class="rounded-circle ma-auto"
+                width="auto"
+                :src="photoSelf">
+                </v-img>
+            </v-col>
+
+            <v-col cols="4">
+                <div class="text-h6 text-md-h5 text-center font-italic">
+                    "Hello, I'm John Knickerbocker! Welcome to my site!"
                 </div>
             </v-col>
+        </v-row>
 
-            <!--to avoid feeling of cramped screen, add some spacing between profile photo and rest of page on xs screens-->
-            <v-col
-            v-if="xs"
-            cols="12">
-                <div style="width: 100%; height: 10vh;"></div>
+        <v-row>
+            <v-col>
             </v-col>
+        </v-row>
 
-            <v-spacer
-            v-else>
-            </v-spacer>
-
-            <v-col
-            order="last"
-            order-md="2"
-            cols="12"
-            sm="12"
-            md="3"
-            lg="3">
+        <v-row
+        class="mr-n6 pr-3 bg-surface"
+        justify="end"
+        style="clip-path: polygon(25% 0%, 100% 0%, 100% 100%, 20% 100%);">
+            <v-col cols="3">
                 <v-carousel
                 class="h-auto"
-                v-model="store.carousel"
+                v-model="store.aboutPage.models.hobbyCarousel"
                 :show-arrows="false"
                 hide-delimiters>
                     <v-carousel-item
-                    v-for="(item, index) in store.items"
-                    :key="item"
+                    v-for="(item, index) in hobbyCarousel"
+                    :key="index"
                     :value="index">
                         <div
-                        class="carousel-description-container custom-scrollbar"
-                        :style="'max-height:' + store.carouselHeight(width) + 'px'">
-                            <div
-                            class="text-body-2"
-                            v-if="store.carousel !== null">
-                                <template v-for="i in 15" :key="i">
-                                    {{ store.items[store.carousel].desc + ' ' }}
-                                </template>
-                            </div>
+                        class="carousel-description-container custom-scrollbar text-body-2">
+                            {{ item.desc }}
                         </div>
                     </v-carousel-item>
                 </v-carousel>
             </v-col>
 
-            <v-col
-            order="3"
-            cols="12"
-            sm="7"
-            md="5"
-            lg="5">
+            <v-col cols="5">
                 <v-carousel
                 class="h-auto"
-                v-model="store.carousel"
+                v-model="store.aboutPage.models.hobbyCarousel"
                 show-arrows="hover"
                 hide-delimiter-background>
                     <v-carousel-item
-                    v-for="(item, index) in store.items"
-                    :key="item"
+                    v-for="(item, index) in hobbyCarousel"
+                    :key="index"
                     :value="index"
                     :src="item.src"
-                    width="100%"
-                    :aspect-ratio="store.aspectRatio">
+                    :aspect-ratio="16/9"
+                    width="auto"
+                    cover>
                     </v-carousel-item>
                 </v-carousel>
             </v-col>

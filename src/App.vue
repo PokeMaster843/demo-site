020dce994ecf12a3b2e7f496b292733a8502a68a
PokeMaster843 <pokemaster843@gmail.com>
2024-01-25 13:11:18 -0500

    VIcon components now use currently selected accent color by default.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <script setup>
+import { ref, onMounted } from "vue";
+
 // project imports
 import TheSidebar from "@/components/TheSidebar.vue";
+import { useThemeStore } from "@/stores/theme";
+
+const store = ref({
+    color: "default"
+});
+onMounted(() => {
+    store.value = useThemeStore();
+});
 </script>
 
 <template>
-    <v-layout style="color: rgb(var(--v-theme-invert));">
-        <the-sidebar></the-sidebar>
+    <v-defaults-provider
+    :defaults="{
+        VIcon: {
+            color: store.color
+        }
+    }">
+        <v-layout>
+            <the-sidebar></the-sidebar>
 
-        <router-view></router-view>
-    </v-layout>
+            <router-view></router-view>
+        </v-layout>
+    </v-defaults-provider>
 </template>
 
 <style scoped>

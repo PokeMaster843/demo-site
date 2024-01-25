80b6a7c8fdbea3e2ae867597d4289b657e7c335f
PokeMaster843 <pokemaster843@gmail.com>
2024-01-25 13:28:51 -0500

    Refactored MyResume page to utilize new 'page' store.

--- src/components/pages/MyResume.vue
+++ src/components/pages/MyResume.vue
@@ -1,35 +1,66 @@
-<script setup>
+<script setup lang="jsx">
 import PageContent from "@/components/PageContent.vue";
-import { resumePage as page } from "@/references";
+import { usePageStore } from "@/stores/page";
+
+const store = usePageStore();
+
+const appBarTabs = [
+    {
+        title: "Education",
+        content: <>
+            <v-card>
+                <div>Test card education</div>
+            </v-card>
+        </>
+    },
+    {
+        title: "Skills",
+        content: <>
+            <v-card>
+                <div>Test card skills</div>
+            </v-card>
+        </>
+    },
+    {
+        title: "Experience",
+        content: <>
+            <v-card>
+                <div>Test card experience</div>
+            </v-card>
+        </>
+    },
+    {
+        title: "Other",
+        content: <>
+            <v-card>
+                <div>Test card other</div>
+            </v-card>
+        </>
+    }
+];
 </script>
 
 <template>
     <page-content
-    :page-title="page.title"
-    :finished="page.finished"
+    :page-title="store.resumePage.title"
+    :finished="store.resumePage.finished"
+    v-model:tabs="store.resumePage.models.appBarTabs"
     include-extension>
         <template #tabs>
-            <v-tab value="education">Education</v-tab>
-            <v-tab value="skills">Skills</v-tab>
-            <v-tab value="experience">Experience</v-tab>
-            <v-tab value="other">Other</v-tab>
+            <v-tab
+            v-for="(item, index) in appBarTabs"
+            :key="index"
+            :value="index">
+                {{ item.title }}
+            </v-tab>
         </template>
 
         <template #window>
-            <v-window-item value="education">
-                Talk about education here
-            </v-window-item>
-
-            <v-window-item value="skills">
-                Talk about skills here
-            </v-window-item>
-
-            <v-window-item value="experience">
-                Talk about experience with technologies (and work experience) here
-            </v-window-item>
-
-            <v-window-item value="other">
-                Another tab
+            <v-window-item
+            v-for="(item, index) in appBarTabs"
+            :key="index"
+            :value="index">
+                <component :is="item.content"></component>
             </v-window-item>
         </template>
     </page-content>
